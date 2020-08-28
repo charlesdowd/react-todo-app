03b8b73bcfc0fb41af915d5c719e29ed067e8419
charlesdowd <charlie.dowd96@gmail.com>
2020-08-28 21:50:39 +0800

    basic functionality working

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
 
+function randomNumberGen() {
+  return Math.random(0,100)
+}
+
 function App() {
   const [ toDoList, setToDoList ] = React.useState([])
   const [ input, setInput ] = React.useState('')
   
   const handleSubmit = () => {
-    setToDoList((toDoList) => toDoList.concat(input))
+    setToDoList((toDoList) => toDoList.concat({
+      id: randomNumberGen(),
+      text: input
+    }))
     setInput('')
   }
 
+  const deleteToDo = (id) => setToDoList((toDoList) => toDoList.filter((t) => t.id !== id))
   return (
     <div className="App">
         <h1>My To Do List</h1>
@@ -24,10 +32,14 @@ function App() {
         onClick={handleSubmit}>Submit</button>
 
         <ul>
-          {toDoList.map((todo) => (
-            <li>
-              {todo}
-            </li>
+          {toDoList.map(({ id, text }) => (
+            <div>
+              {console.log({id})}
+              <li key={id}>
+                <span>{text}</span>
+              </li>
+              <button onClick={() => deleteToDo(id)}>X</button>  
+            </div>
           ))}
         </ul>
     </div>
